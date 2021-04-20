d62737d6fd8adf8ebf901678fde696245ee6c6b9
Ansagan <ansagankabdolla4@gmail.com>
2021-04-20 15:15:21 +0600

    101 commit

--- lab/api/v1/endpoints/order.py
+++ lab/api/v1/endpoints/order.py
@@ -131,12 +131,12 @@ async def get_order_group():
     for i in query:
         a = i.__dict__
         zero = 0
-        for j in query.orders.all():
+        for j in i.orders.all():
             c = len(j.cells)
             zero += c
 
         a["cellsCount"] = zero
-        a["orderCount"] = len(query.orders.all())
+        a["orderCount"] = len(i.orders.all())
     return query
 
 
