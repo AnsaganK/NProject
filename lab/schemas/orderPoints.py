771c7648d4d2bf697d01cece0dd191397f871a8c
Ansagan <ansagankabdolla4@gmail.com>
2021-04-22 14:53:08 +0600

    103 commit

--- lab/schemas/orderPoints.py
+++ lab/schemas/orderPoints.py
@@ -5,11 +5,13 @@ from pydantic.types import List
 class BaseOrderPointsSchema(BaseModel):
     orderId: int
     orderGroupId: int
-    points: str
+    points: List[dict]
     dateCreate: int
+
 
 class OrderPointsSchema(BaseOrderPointsSchema):
     pass
+
 
 class CreateOrderPointsSchema(BaseOrderPointsSchema):
     id: int
