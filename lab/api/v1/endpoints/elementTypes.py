71443985157112e3059dd39dd5fdc29d34b6a89d
Ansagan <ansagankabdolla4@gmail.com>
2021-05-05 15:26:20 +0600

    050521

--- lab/api/v1/endpoints/elementTypes.py
+++ lab/api/v1/endpoints/elementTypes.py
@@ -37,7 +37,7 @@ async def create_element_type(type_id: int, elements: ElementForTypeSchema):
         if not i.date:
             element.date = int(time.time())
         elementType = ElementType(element=element, type=query)
-        for j in i.ranges:
+        for j in i.range:
             range_name = j.name
             range_of = j.of
             range_to = j.to
