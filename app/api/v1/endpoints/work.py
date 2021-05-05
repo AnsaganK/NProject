d9efdd3ab4802336e8dc226955ade10269c63d28
Ansagan <ansagankabdolla4@gmail.com>
2021-05-05 09:11:05 +0600

    050521

--- app/api/v1/endpoints/work.py
+++ app/api/v1/endpoints/work.py
@@ -23,7 +23,7 @@ async def get_works():
     query = session.query(Work).options(selectinload(Work.field)).all()
     return query
 
-@router.get("employee/{employee_id}")
+@router.get("/employee/{employee_id}")
 async def get_works_for_employee(employee_id:int):
     employee = session.query(User).filter(employee_id).first()
     if not employee:
