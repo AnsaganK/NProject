0f74b8baaad059e3bc1af5c84ed0857996f7fe22
Ansagan <ansagankabdolla4@gmail.com>
2021-05-06 09:29:56 +0600

    060521

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
 
-#DB_USER = "admin"
-#DB_PASSWORD = "4SvjIp54HhTN"
-#DB_HOST = "127.0.0.1"
-#DB_NAME = "navistar"
-
-DB_USER = "postgres"
-DB_PASSWORD = "12345"
+DB_USER = "admin"
+DB_PASSWORD = "4SvjIp54HhTN"
 DB_HOST = "127.0.0.1"
-DB_NAME = "Navistar"
+DB_NAME = "navistar"
+
+#DB_USER = "postgres"
+#DB_PASSWORD = "12345"
+#DB_HOST = "127.0.0.1"
+#DB_NAME = "Navistar"
 
 DATABASE_URL = (
     f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:5432/{DB_NAME}"
