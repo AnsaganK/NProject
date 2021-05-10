12c40d95c0e4f026ecbe433ca400cb8fbbdf7d20
Ansagan <ansagankabdolla4@gmail.com>
2021-05-11 04:28:10 +0600

    1105

--- lab/api/v1/endpoints/elementTypes.py
+++ lab/api/v1/endpoints/elementTypes.py
@@ -79,8 +79,7 @@ async def create_element_type(type: ElementTypeSchema):
 
 @router.delete("/{type_id}")
 async def delete_element_type(type_id: int):
-    query = session.query(ElementType).filter(ElementType.id == type_id).first()
-    print(query)
+    query = session.query(Type).filter(Type.id == type_id).first()
     if query:
         session.delete(query)
         session.commit()
