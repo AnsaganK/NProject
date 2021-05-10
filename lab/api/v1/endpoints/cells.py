73a43edc3b4fc0d52e417184695ba9737e2cf8b8
Ansagan <ansagankabdolla4@gmail.com>
2021-05-10 11:18:26 +0600

    fix element name

--- lab/api/v1/endpoints/cells.py
+++ lab/api/v1/endpoints/cells.py
@@ -51,7 +51,6 @@ async def get_results():
 async def get_cells_for_order(order_id: int):
     cells = session.query(OrderCellsResult).options(selectinload(OrderCellsResult.element)).join(OrderCells).filter(OrderCells.orderId == order_id).all()
     elementTypes = session.query(ElementType).join(OrderElementsType).filter(OrderElementsType.c.orderId == order_id).all()
-    print(elementTypes)
     dic = []
     for cell in cells:
         cellCode = cell.orderCell.cell.code
@@ -59,6 +58,10 @@ async def get_cells_for_order(order_id: int):
         for c in cells:
             if c.orderCell.cell.code == cellCode:
                 el = c.element
+                elType = elementTypes.filter(ElementType.elementId == el.id)
+                print(12)
+                print(elType)
+                #errorRanges =
                 cellDic["results"].append({"element": {"id":el.id, "code": el.code, "name": el.name}, "value": c.result})
         if cellDic not in dic:
             dic.append(cellDic)
