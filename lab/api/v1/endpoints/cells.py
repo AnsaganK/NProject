5a4820c8ba362af5e6a535d72919e49c0eb26aa7
Ansagan <ansagankabdolla4@gmail.com>
2021-05-10 11:13:02 +0600

    fix element name

--- lab/api/v1/endpoints/cells.py
+++ lab/api/v1/endpoints/cells.py
@@ -50,6 +50,8 @@ async def get_results():
 @router.get("/results/{order_id}")
 async def get_cells_for_order(order_id: int):
     cells = session.query(OrderCellsResult).options(selectinload(OrderCellsResult.element)).join(OrderCells).filter(OrderCells.orderId == order_id).all()
+    elementTypes = session.query(OrderElementsType).filter(OrderElementsType.orderId == order_id).all()
+    print(elementTypes)
     dic = []
     for cell in cells:
         cellCode = cell.orderCell.cell.code
