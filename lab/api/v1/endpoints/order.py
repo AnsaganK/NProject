65778c2584b8c0a58d6d8040b892e7c191b107fd
Ansagan <ansagankabdolla4@gmail.com>
2021-06-21 18:14:43 +0600

    2106_2 commit

--- lab/api/v1/endpoints/order.py
+++ lab/api/v1/endpoints/order.py
@@ -112,8 +112,9 @@ async def get_order_group_id(group_id: int):
         if i.field:
             i.__dict__['fieldName'] = i.field.name
     for i in query.elementTypes:
-        i.__dict__["elementName"] = i.element.name
-        del i.__dict__["element"]
+        if i.element:
+            i.__dict__["elementName"] = i.element.name
+            del i.__dict__["element"]
     if query:
         return query
     return {"error": "Not Found"}
